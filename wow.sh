ace99ad910323f7aab1083022be39a7d1c107f1f
Bill Fern <bfern@nerdery.com>
2014-10-16 12:26:42 -0500

    The real commit

---
+++ wow.sh
@@ -0,0 +1,18 @@
+COMMIT_DATE="2014-10-16"
+for j in {1..365}
+do
+    COMMIT_DATE=$(gdate "--date=$COMMIT_DATE - 1 days" +%Y-%m-%d)
+    COMMIT_AMOUNT=0
+    while [ "$COMMIT_AMOUNT" -le 20 ]
+    do
+        COMMIT_AMOUNT=$RANDOM
+        let "COMMIT_AMOUNT %= 300"
+    done
+    for i in $(eval echo "{1..$COMMIT_AMOUNT}")
+    do
+        cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 >> cv.txt
+        git commit -am "watch this" --date=$COMMIT_DATE
+    done
+    git push
+    rm README.md
+done
